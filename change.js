c6f1e7c16517c25290f1a7070350fccf71409c65
N1kh1lR <42893202+Nikhilrathoure@users.noreply.github.com>
2018-09-13 18:26:37 +0530

    second time

--- change.js
+++ change.js
@@ -5,4 +5,5 @@ const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'
 function change() {      
 document.body.style.background = rainbow[Math.floor(7*Math.random())];
 }
+
 btn.addEventListener('click', change);
